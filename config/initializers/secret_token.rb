d5f37826f5257ee304c4fc4684a657e6f6895c8a
Matthew Carlone <matthewcarlone@gmail.com>
2010-11-07 13:38:44 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dorfman::Application.config.secret_token = '1afa4a362ee47021b8a3a4f718254d6652dcdd32ba1e3952ef8bb3d30a9baf1ff1bb842dc7b37c3170620a90e96fbc7282c6eced74c7cbdf665391ef33b8f518'
