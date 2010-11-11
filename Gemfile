0447bd0604b1ec62890632cfc435aa98aa364180
Matthew Carlone <matthewcarlone@gmail.com>
2010-11-11 00:04:00 -0500

    Add Compass and Sass.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'devise'
 
 gem 'will_paginate', '>= 3.0.pre2'
 gem 'paperclip', '2.3.3'
+gem "compass", ">= 0.10.6"
 gem 'haml-rails'
 gem 'mocha'
 gem "capybara"
---
+++ config/initializers/stylesheets.rb
@@ -0,0 +1,16 @@
+# Adapted from
+# http://github.com/chriseppstein/compass/issues/issue/130
+# and other posts.
+
+# Create the dir
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Sass::Plugin.on_updating_stylesheet do |template, css|
+  puts "Compiling #{template} to #{css}"
+end
+Sass::Plugin.options[:never_update] = true
+
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+#                                              :urls => ['/stylesheets'],
+#                                              :root => "#{Rails.root}/tmp")
