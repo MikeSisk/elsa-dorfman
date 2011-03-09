6684188fd2cc50950815fda8ee79afe9e6bd4672
Matthew Carlone <matthewcarlone@gmail.com>
2011-03-09 01:56:12 -0500

    Add medium image size test page element.

--- app/models/album_page_element.rb
+++ app/models/album_page_element.rb
@@ -1,7 +1,7 @@
 class AlbumPageElement < ActiveRecord::Base
   validates :style, :presence => true
 
-  @@Styles = %w[test1 test2 test3]
+  @@Styles = %w[test1 test2 test3 test4]
   def self.styles
     @@Styles
   end
