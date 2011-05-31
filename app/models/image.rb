ca57b596ca3a32ae3a85d21f95b9c468bfd0dca2
Matthew Carlone <matthewcarlone@gmail.com>
2011-05-31 06:50:23 -0400

    Add small size new dimensions.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   
   has_attached_file :resource, 
-    :styles => { :thumb => "128x128#", :small => "128x200>", :medium => "256x256>" },
+    :styles => { :thumb => "128x128#", :small => "87x119>", :medium => "256x256>" },
     :storage => :s3,
     :bucket => 'dorfman-development',
     :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] },
