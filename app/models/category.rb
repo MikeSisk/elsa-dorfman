5bd302217484e28e9abd6b4e62b67a72a118a373
Matthew Carlone <matthewcarlone@gmail.com>
2011-07-28 12:34:31 -0400

    Left side-bar menu graphics. Ordering main nav tabs.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   has_many :albums
  # validates_associated :album
  
- default_scope order("position ASC")
+ default_scope order("categories.position ASC")
  
   def first_album
     albums.empty? ? '#' : albums.first
