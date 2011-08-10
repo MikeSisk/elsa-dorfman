7be2e7bf7d85fc009a20227ff6312beb4f477ef0
Matthew Carlone <matthewcarlone@gmail.com>
2011-08-10 16:33:42 -0400

    Change Image :medium size.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,8 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   
   has_attached_file :resource, 
-    :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "256x256>" },
+    # :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "256x256>" },
+    :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "181x256>" },
     :storage => :s3,
     :bucket => 'elsadorfman-production',
     :s3_credentials => { :access_key_id => ENV['DORFMAN_S3_KEY'], :secret_access_key => ENV['DORFMAN_S3_SECRET'] },
