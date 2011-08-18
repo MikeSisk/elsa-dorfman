9fc7f26976583d3dba3965ec1a7cbb59b584bc77
Matthew Carlone <matthewcarlone@gmail.com>
2011-08-18 15:46:40 -0400

    Image resize.

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,7 @@ class Image < ActiveRecord::Base
   
   has_attached_file :resource, 
     # :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "256x256>" },
-    :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "181x256>", :inline => "x256" },
+    :styles => { :thumb => "128x128#", :small => "87x119#", :medium => "181x256>", :inline => "x256>" },
     :storage => :s3,
     :bucket => 'elsadorfman-production',
     :s3_credentials => { :access_key_id => ENV['DORFMAN_S3_KEY'], :secret_access_key => ENV['DORFMAN_S3_SECRET'] },
