60268c3db8b5909970d8e3881d3de9734f119dd7
Matthew Carlone <matthewcarlone@gmail.com>
2012-01-25 00:59:46 -0500

    Screwed it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,9 +28,7 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @page }
-    end
-  end
+      format.xml  { render :xml => @page } end end
 
   # GET /pages/1/edit
   def edit
